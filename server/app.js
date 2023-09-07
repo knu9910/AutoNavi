06c5afff1e1c9fd3b35d57668faf6f63f3d4c29f
knu9910 <knu9910@naver.com>
2023-09-07 15:42:17 +0900

    Complete Socket.io configuration
    socket.io 설정 완료 및, 클라이언트에 실시간으로 데이터 뿌려주기 완료

--- server/app.js
+++ server/app.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-var app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index');
 
-app.use(logger('dev'));
+const watchDatabaseChanges = require('./carHelperApis/watchDatabaseChanges');
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
 
+io.on('connection', (socket) => {
+  console.log('사용자가 연결 되었습니다.');
+  watchDatabaseChanges(io);
+
+  socket.on('disconnect', () => {
+    console.log('사용자가 연결 해제되었습니다.');
+  });
+});
+
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
 module.exports = app;
