3762576dc8f5afe3cb5f38b7b3cb35382ee83177
cherrie <cherry199848@gmail.com>
2023-09-12 15:18:11 +0900

    Admin user registration page, login page updated

--- client/src/pages/auth/AdminReg.js
+++ client/src/pages/auth/AdminReg.js
@@ -1,5 +1,107 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+
 const AdminReg = () => {
-  return <div>AdminReg</div>;
+  const [userName, setUserName] = useState('');
+  const [userIdReg, setUserIdReg] = useState('');
+  const [passwordReg, setPasswordReg] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [position, setPosition] = useState('');
+  const [controlRights, setControlRights] = useState('');
+
+  const register = () => {
+    // 비밀번호와 비밀번호 확인이 일치하는지 check
+    if (passwordReg !== confirmPassword) {
+      alert('비밀번호를 똑같이 입력해주세요.');
+      return;
+    }
+
+    Axios.post('http://localhost:8080/users/register', {
+      userName: userName,
+      userId: userIdReg,
+      password: passwordReg,
+      email: email,
+      position: position,
+      controlRights: controlRights,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  return (
+    <div className="register">
+      <input
+        type="text"
+        placeholder="이름"
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      />
+
+      <input
+        type="text"
+        placeholder="사원코드"
+        onChange={(e) => {
+          setUserIdReg(e.target.value);
+        }}
+      />
+
+      <input
+        type="password"
+        placeholder="비밀번호"
+        onChange={(e) => {
+          setPasswordReg(e.target.value);
+        }}
+      />
+
+      <input
+        type="password"
+        placeholder="비밀번호 확인"
+        onChange={(e) => {
+          setConfirmPassword(e.target.value);
+        }}
+      />
+
+      <input
+        type="email"
+        placeholder="이메일"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+
+      <input
+        type="text"
+        placeholder="직급"
+        onChange={(e) => {
+          setPosition(e.target.value);
+        }}
+      />
+      <p>차량 제어 권한</p>
+      <input
+        type="radio"
+        name="controlRights"
+        value="있음"
+        id="yes"
+        onChange={(e) => {
+          setControlRights(e.target.value);
+        }}
+      />
+      <label htmlFor="yes">있음</label>
+      <input
+        type="radio"
+        name="controlRights"
+        value="없음"
+        id="no"
+        onChange={(e) => {
+          setControlRights(e.target.value);
+        }}
+      />
+      <label htmlFor="no">없음</label>
+      <button onClick={register}>관리자 등록</button>
+    </div>
+  );
 };
 
 export default AdminReg;
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,6 +1,31 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+
 import mainLogo from '../../img/autoNavi_Logo.jpg';
+// import '../../css/home.css';
 
 const Login = () => {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = () => {
+    Axios.post('http://localhost:8080/users/login', {
+      userId: userId,
+      password: password,
+    })
+      .then((response) => {
+        if (response.data.message === 'Login successful') {
+          console.log(response.data.message);
+        } else {
+          alert('id 또는 비밀번호를 잘못 입력했습니다.');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        alert('id 또는 비밀번호를 잘못 입력했습니다.');
+      });
+  };
+
   return (
     <div className="mainpage">
       <div className="Logo">
@@ -9,8 +34,34 @@ const Login = () => {
       <div className="font-fix">
         <p className="font">무인 운송차량 관제 시스템</p>
       </div>
-      <div className="buttontaget">
-        <button className="main_btn">Sign in</button>
+
+      <div className="wrapper">
+        <div className="container">
+          <div className="sign-in-container">
+            <form className="signinDetail">
+              <p>Sign In</p>
+              <input
+                type="id"
+                placeholder="Admin ID"
+                onChange={(e) => {
+                  setUserId(e.target.value);
+                }}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <div className="buttontarget">
+                <button className="main_btn" onClick={login}>
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
