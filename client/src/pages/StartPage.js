c47b5ba4eacf268896c5efa5118e5feaa82c3085
ShinGyuRim <future0226@naver.com>
2023-09-12 22:37:42 +0900

    2023/09/12 startpage

--- client/src/pages/StartPage.js
+++ client/src/pages/StartPage.js
@@ -1,5 +1,26 @@
+import React from 'react';
+import '../styles/startPage.css';
+import AutoNaviLogo from '../img/autoNavi_Logo.jpg';
+import { Link } from 'react-router-dom';
+
 const StartPage = () => {
-  return <div>StartPage</div>;
+  return (
+    <>
+      <div className="mainpage">
+        <div className="Logo">
+          <img className="Logoimg" src={AutoNaviLogo} alt="" />
+        </div>
+        <div className="font-fix">
+          <p className="font">무인 운송차량 관제 시스템</p>
+        </div>
+        <div className="buttontaget">
+          <Link to="/signin">
+            <button className="main_btn">Sign in</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default StartPage;
