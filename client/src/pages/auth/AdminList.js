ff0d668069606e6ab86437430044790bb4817d5c
ShinGyuRim <future0226@naver.com>
2023-09-13 11:24:47 +0900

    2023.09.13 adminlist js,css

--- client/src/pages/auth/AdminList.js
+++ client/src/pages/auth/AdminList.js
@@ -1,5 +1,91 @@
+import '../../styles/adminList.css';
+import blueLogo from '../../img/bluelogo.jpg';
+import React from 'react';
+import { Link } from 'react-router-dom';
 const AdminList = () => {
-  return <div>AdminList</div>;
+  return (
+    <>
+      <header className="ContactPersonList">
+        <img className="small-logo" src={blueLogo} alt="" />
+      </header>
+
+      <div className="cp-search-box">
+        <div className="cp-box1">
+          <button className="cp-button1" href="#">
+            <p>관리자 등록</p>
+          </button>
+        </div>
+        <div className="cp-box2">
+          <form className="cp-search">
+            <input
+              className="cp-text"
+              type="text"
+              name=""
+              placeholder="Search..."
+            />
+            <button className="cp-button" type="submit">
+              <i className="fa fa-search" aria-hidden="true"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="cp-container">
+        <table className="cp-table">
+          <thead>
+            <tr>
+              <th>사원번호</th>
+              <th>이름</th>
+              <th>직급</th>
+              <th>권한</th>
+              <th>...</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>726228</td>
+              <td>신규림</td>
+              <td>사원</td>
+              <td>A</td>
+              <td style={{ float: 'right' }}>
+                <button className="cp-edit" href="#">
+                  수정
+                </button>
+                <button className="cp-delete" href="#">
+                  삭제
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>726228</td>
+              <td>신규림</td>
+              <td>사원</td>
+              <td>A</td>
+              <td style={{ float: 'right' }}>
+                <button className="cp-edit" href="#">
+                  수정
+                </button>
+                <button className="cp-delete" href="#">
+                  삭제
+                </button>
+              </td>
+            </tr>
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td colSpan="5" className="text-center">
+                autonavi@autonavi.kr{' '}
+                <Link to="" target="_blank">
+                  autonavi
+                </Link>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default AdminList;
