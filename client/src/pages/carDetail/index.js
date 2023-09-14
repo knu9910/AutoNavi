87d6f1b4be2746c2a34ca8f5ae4728986587f46d
DESKTOP-R2CREDQ\Beomhyun <cmpsr123@naver.com>
2023-09-14 16:58:07 +0900

    design: cardetail css 추가

--- client/src/pages/carDetail/index.js
+++ client/src/pages/carDetail/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useDaumPostcodePopup } from 'react-daum-postcode';
 import { useParams } from 'react-router';
+import '../../styles/carDetail.css';
 
 const CarDetail = () => {
   const open = useDaumPostcodePopup();
@@ -27,11 +28,46 @@ const CarDetail = () => {
     });
   };
   return (
-    <div>
-      <input />
-      <button onClick={() => open({ onComplete: (data) => handleClick(data) })}>
-        목적지 검색
-      </button>
+    <div className="car_detail_wrap">
+      <div className="car_info">1234 차량정보</div>
+      <div className="car_info_wrap">
+        <div className="car_info_inner">
+          <div className="info_label">차량번호</div>
+          <div className="info_box"></div>
+          <div className="info_label">차종</div>
+          <div className="info_box"></div>
+          <div className="info_label">배터리</div>
+          <div className="info_box"></div>
+          <div className="info_label">운행 현황</div>
+          <div className="info_box"></div>
+        </div>
+      </div>
+      <div className="input_box">
+        <div className="icon"></div>
+        <div className="origin_wrap">
+          <input
+            className="origin"
+            type="text"
+            placeholder="출발지"
+            readOnly
+          ></input>
+        </div>
+        <div className="destination_wrap">
+          <input
+            className="destination"
+            type="text"
+            placeholder="목적지를 검색하세요"
+          ></input>
+        </div>
+      </div>
+      <div className="search_wrap">
+        <button
+          className="search"
+          onClick={() => open({ onComplete: (data) => handleClick(data) })}
+        >
+          목적지 검색
+        </button>
+      </div>
     </div>
   );
 };
