32aff239b5b0e40f7e81cbb90f407448f4cf0b83
knu9910 <knu9910@naver.com>
2023-09-29 01:57:10 +0900

    feat: History 전체 조회, 업데이트, 상세조회 api 작성

--- server/models/historyCar_model.js
+++ server/models/historyCar_model.js
@@ -31,6 +31,48 @@ const historyCarModel = {
     const [rows] = await pool.query(sql, [preDate, nextDate]);
     return rows;
   },
+  async historyUpdate(id, article) {
+    const sql = `UPDATE car_history SET
+      ${article.accident !== undefined ? 'accident = ?,' : ''}
+      ${article.cum_distance !== undefined ? 'cum_distance = ?,' : ''}
+      ${article.cum_battery !== undefined ? 'cum_battery = ?,' : ''}
+      ${article.tire_change !== undefined ? 'tire_change = ?,' : ''}
+      ${article.battery_change !== undefined ? 'battery_change = ?,' : ''}
+      updatedAt = NOW()
+      WHERE car_id = ?;
+    `;
+
+    // 업데이트할 속성 값들을 추출
+    const values = [
+      article.accident,
+      article.cum_distance,
+      article.cum_battery,
+      article.tire_change,
+      article.battery_change,
+      id,
+    ];
+
+    // SQL 쿼리와 바인딩할 값을 반환
+    const [rows] = await pool.query(sql, values);
+    return rows;
+  },
+
+  async getCarAllHistory() {
+    const sql = `SELECT * FROM car_history`;
+    const [history] = await pool.query(sql);
+    return history;
+  },
+
+  async getCarHistoryByCarId(carId) {
+    const sql = `
+      SELECT *
+      FROM car_history
+      WHERE car_id = ?
+    `;
+
+    const [rows] = await pool.query(sql, [carId]);
+    return rows;
+  },
 };
 
 module.exports = historyCarModel;
--- server/routes/historyCar.js
+++ server/routes/historyCar.js
@@ -48,4 +48,44 @@ router.post('/chargePost', async (req, res) => {
   }
 });
 
+router.put('/updateHistory/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const article = req.body;
+
+    // historyUpdate 함수를 호출하여 업데이트를 수행하고 결과를 받습니다.
+    const updatedRows = await historyCarModel.historyUpdate(id, article);
+
+    res
+      .status(200)
+      .json({ message: 'History updated successfully', updatedRows });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: '데이터베이스 오류' });
+  }
+});
+
+router.get('/getAllHistory', async (req, res) => {
+  try {
+    // getCarAllHistory 함수를 호출하여 모든 히스토리 데이터를 가져옵니다.
+    const history = await historyCarModel.getCarAllHistory();
+    res.status(200).json(history);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: '데이터베이스 오류' });
+  }
+});
+
+router.get('/getHistoryByCar/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    // getCarHistoryByCarId 함수를 호출하여 차량별 히스토리 데이터를 가져옵니다.
+    const history = await historyCarModel.getCarHistoryByCarId(id);
+    res.status(200).json(history);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: '데이터베이스 오류' });
+  }
+});
+
 module.exports = router;
