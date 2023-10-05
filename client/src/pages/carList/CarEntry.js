ff8ae739a2f0d936dbc4a0267990a5ac27a69d68
knu9910 <knu9910@naver.com>
2023-10-06 07:01:14 +0900

    fix: 차량 이동거리, 차량 배터리 사용량 단위 추가

--- client/src/pages/carList/CarEntry.js
+++ client/src/pages/carList/CarEntry.js
@@ -1,16 +1,17 @@
 import { Link } from 'react-router-dom';
+import { metersToKMAndM } from '../../helperFunction';
 
 const CarEntry = ({ car, history }) => {
   const { operation_st, car_name, id } = car;
-
+  const cum_distance = metersToKMAndM(history.cum_distance);
   return (
     <div className="card-1">
       <li className="car">
         <Link className="carlist-info" to={`/car/detail/${id}`}>
           <p className="businfo">{operation_st}</p>
           <p className="businfo">{car_name}</p>
-          <p className="businfo">{history.cum_distance}</p>
-          <p className="businfo">{history.cum_battery}</p>
+          <p className="businfo">{cum_distance}</p>
+          <p className="businfo">{history.cum_battery} Kwh</p>
         </Link>
       </li>
     </div>
