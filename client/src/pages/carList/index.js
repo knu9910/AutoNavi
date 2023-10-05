b273b8a59f5bf29e3c2acd217192c1d90869f47f
knu9910 <knu9910@naver.com>
2023-10-06 01:19:28 +0900

    feat: List 페이지에서 CarsHistory 상태 업데이트

--- client/src/pages/carList/index.js
+++ client/src/pages/carList/index.js
@@ -1,8 +1,29 @@
+import { useDispatch } from 'react-redux';
 import '../../styles/carList.css';
 import List from './List';
 import CarDashBoard from './dashboard/index';
+import axios from 'axios';
+import { getCarsHistorys } from '../../store/historySlice';
+import { useEffect } from 'react';
 
 const CarList = () => {
+  const dispatch = useDispatch();
+
+  const handleCarsHistorys = async () => {
+    try {
+      const res = await axios.get(
+        'http://localhost:8080/api/history/getAllHistory',
+      );
+      const historys = res.data;
+      dispatch(getCarsHistorys({ historys }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    handleCarsHistorys();
+  }, []);
   return (
     <>
       <section>
