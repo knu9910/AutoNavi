e7f03ffd6affa373424a7ca444d02b957d85c396
knu9910 <knu9910@naver.com>
2023-10-11 10:12:53 +0900

    fix: 새로고침시 OperationState 그래프가 나오지 않던 버그 수정

--- client/src/pages/carList/dashboard/OpreationState.js
+++ client/src/pages/carList/dashboard/OpreationState.js
@@ -3,34 +3,26 @@ import 'chart.js';
 import Chart from 'chart.js/auto';
 import { useSelector } from 'react-redux';
 
-const OpreationState = () => {
+const OperationState = () => {
   const doughnutChartRef = useRef(null);
 
-  const [showDistanceCharts] = useState(false);
   const [chartInstances, setChartInstances] = useState({
     doughnutChart: null,
   });
 
   const carsInfo = useSelector((state) => state.carStore.carsInfo);
-  const waitingcars = carsInfo.filter(
-    (waitingCar) => waitingCar.operation_st === '대기',
-  );
-  const operationCars = carsInfo.length - waitingcars.length;
-  const waitCars = waitingcars.length;
 
-  useEffect(() => {
-    const { doughnutChart } = chartInstances;
+  const waitingCars = carsInfo.filter((car) => car.operation_st === '대기');
+  const operationCars = carsInfo.length - waitingCars.length;
+  const waitingCarCount = waitingCars.length;
 
-    if (doughnutChart) {
-      doughnutChart.destroy();
-    }
-
-    if (doughnutChartRef.current) {
+  const initializeChart = (ref) => {
+    if (ref.current) {
       const data = {
         labels: ['대기', '운행'],
         datasets: [
           {
-            data: [waitCars, operationCars],
+            data: [waitingCarCount, operationCars],
             backgroundColor: ['#0300a5', '#D7F0FF'],
             cutout: '65%',
           },
@@ -41,18 +33,37 @@ const OpreationState = () => {
         plugins: {},
       };
 
-      const doughnutChart = new Chart(doughnutChartRef.current, {
+      return new Chart(ref.current, {
         type: 'doughnut',
         data: data,
         options: options,
       });
-
-      setChartInstances((prevInstances) => ({
-        ...prevInstances,
-        doughnutChart,
-      }));
     }
-  }, [showDistanceCharts]);
-  return <canvas className="ochart" ref={doughnutChartRef}></canvas>;
+    return null;
+  };
+
+  useEffect(() => {
+    const { doughnutChart } = chartInstances;
+
+    if (doughnutChart) {
+      doughnutChart.destroy();
+    }
+
+    const newDoughnutChart = initializeChart(doughnutChartRef);
+
+    setChartInstances((prevInstances) => ({
+      ...prevInstances,
+      doughnutChart: newDoughnutChart,
+    }));
+
+    return () => {
+      if (newDoughnutChart) {
+        newDoughnutChart.destroy();
+      }
+    };
+  }, [waitingCarCount, operationCars]);
+
+  return <canvas className="operation-chart" ref={doughnutChartRef}></canvas>;
 };
-export default OpreationState;
+
+export default OperationState;
