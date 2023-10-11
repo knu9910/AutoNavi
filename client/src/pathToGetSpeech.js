5caaa616899bdc08269b9547ae5ce0ac3e0e15ff
knu9910 <knu9910@naver.com>
2023-10-11 10:03:10 +0900

    fix: 차량 운행 초기에 음성이 나오지 않는 버그 수정

--- client/src/pathToGetSpeech.js
+++ client/src/pathToGetSpeech.js
@@ -1,41 +1,41 @@
 export const getSpeech = (text) => {
-  let voices = [];
-
-  //디바이스에 내장된 voice를 가져온다.
-  const setVoiceList = () => {
-    voices = window.speechSynthesis.getVoices();
+  // 음성 데이터 로드
+  const loadVoices = async () => {
+    return new Promise((resolve) => {
+      const intervalId = setInterval(() => {
+        const voices = window.speechSynthesis.getVoices();
+        if (voices.length > 0) {
+          clearInterval(intervalId);
+          resolve(voices);
+        }
+      }, 100);
+    });
   };
 
-  setVoiceList();
-
-  if (window.speechSynthesis.onvoiceschanged !== undefined) {
-    //voice list에 변경됐을때, voice를 다시 가져온다.
-    window.speechSynthesis.onvoiceschanged = setVoiceList;
-  }
-
-  const speech = (txt) => {
+  // 음성 합성 함수
+  const speak = async (txt) => {
     const lang = 'ko-KR';
-    const utterThis = new SpeechSynthesisUtterance(txt);
+    const voices = await loadVoices();
 
-    utterThis.lang = lang;
-
-    /* 한국어 vocie 찾기
-         디바이스 별로 한국어는 ko-KR 또는 ko_KR로 voice가 정의되어 있다.
-      */
+    // 한국어 voice 찾기
     const kor_voice = voices.find(
       (elem) => elem.lang === lang || elem.lang === lang.replace('-', '_'),
     );
 
-    //힌국어 voice가 있다면 ? utterance에 목소리를 설정한다 : 리턴하여 목소리가 나오지 않도록 한다.
     if (kor_voice) {
+      const utterThis = new SpeechSynthesisUtterance(txt);
+      utterThis.lang = lang;
       utterThis.voice = kor_voice;
+      window.speechSynthesis.speak(utterThis);
     } else {
-      return;
+      console.error('한국어 목소리를 찾을 수 없습니다.');
     }
-
-    //utterance를 재생(speak)한다.
-    window.speechSynthesis.speak(utterThis);
   };
 
-  speech(text);
+  // 브라우저에서 SpeechSynthesis 및 오디오 권한 확인
+  if ('speechSynthesis' in window && 'SpeechSynthesisUtterance' in window) {
+    speak(text);
+  } else {
+    console.error('음성 합성을 지원하지 않습니다.');
+  }
 };
